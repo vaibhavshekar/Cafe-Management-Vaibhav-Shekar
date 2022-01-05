d292b1d75081c7c94f16f55cd528c3153860035a
Vaibhav Shekar <88209015+vaibhavshekar@users.noreply.github.com>
2022-01-05 06:12:43 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,50 +1,36 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan  4 22:08:30 2022
-
-@author: Administrator
-"""
 from tkinter import *
 from datetime import datetime
 import tkinter as tk 
 import mysql.connector as contr
 import pandas as pd
 import csv 
-
-mydb = contr.connect(
-  host="localhost",
-  user="root",
-  password="123456",
-  database="Cafe_Management"
-)
-
-global mycursor
-mycursor= mydb.cursor()
   
 root=tk.Tk() 
 root.geometry("600x400")
-  
-card_var=tk.StringVar()
+card_var=StringVar()
   
 def submit():
- 
+
     cardno=card_var.get()
     timestamp = str(datetime.now())
     data = [cardno,timestamp]
     df1 = pd.read_csv('E:\Cafe Management\saved cards.csv',usecols = ['card number'])
-    exist = cardno in df1
-    correct = exist
-    if card_var in df1.values :
-        df = pd.read_csv('E:\Cafe Management\punch.csv',usecols = ['punch details','card number'])
-        df2 = pd.DataFrame(data)
+    lst = df1['card number'].tolist()
+    intcdno = int(cardno)
+    if (intcdno in lst):        
+        df = pd.read_csv('E:\Cafe Management\punch.csv',usecols = ['card number','punch details',])
+        df2 = pd.DataFrame({'card number':[cardno],'punch details':[timestamp]})
         df3 = df.append(df2,ignore_index = True)
         df3.to_csv('E:\Cafe Management\punch.csv',index=False)
+        card_var.set("")
     else:
         messagebox.showerror("showerror", "Error: Invalid Card number")
 
-    card_var.set("")
+        card_var.set("")
     
-    #root.destroy()
+    root.destroy()
+    
+    
      
 name_label = tk.Label(root, text = 'CARD Number', font=('calibre',10, 'bold'))
 name_entry = tk.Entry(root,textvariable = card_var, font=('calibre',10,'normal'))
